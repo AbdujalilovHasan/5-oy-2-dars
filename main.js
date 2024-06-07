629d7880c776d302221cbb46da3bdc19955546cf
AbdujalilovHasan <abdujalilovhasan171@gmaile.com>
2024-06-07 14:50:49 +0500

    first commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let ism = prompt('Ism kiriting')
-let id = prompt('Space id kiriting')
-let pas = prompt('Space password kiriting')
+let ism = prompt('Ism kiriting');
+let id = prompt('Space id kiriting');
+let pas = prompt('Space password kiriting');
 
 let obj = {
     name: ism,
